039264a32a85ad180a1efe3e95ee7983b8ed4e03
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-05-11 20:16:36 +0330

    implement cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart_detail_view, name='cart-detail'),
+    path('add/<int:product_id>/', views.add_to_cart_view, name='cart-add'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='cart-remove'),
 ]
 
