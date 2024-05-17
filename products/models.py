626907074921651bfff4c0d8ee5806752baa9ceb
mohammad ghorashi <mohammadghorashi81@gmail.com>
2024-05-17 12:11:20 +0330

    add short_description field to product model [set verboss name for Product Model]

--- products/models.py
+++ products/models.py
@@ -7,14 +7,15 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = RichTextField()
-    price = models.PositiveIntegerField(default=0)
-    active = models.BooleanField(default=True)
+    title = models.CharField(verbose_name=_('Title'), max_length=100)
+    description = RichTextField(verbose_name=_('Description'))
+    short_description = models.TextField(verbose_name=_('Short Description'), blank=True)
+    price = models.PositiveIntegerField(verbose_name=_('Price'), default=0)
+    active = models.BooleanField(verbose_name=_('Active'), default=True)
     image = models.ImageField(verbose_name=_('Product Image'), upload_to='product/product_cover/', blank=True)
 
-    datetime_created = models.DateTimeField(default=timezone.now)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_created = models.DateTimeField(verbose_name=_('Datetime Created'), default=timezone.now)
+    datetime_modified = models.DateTimeField(verbose_name=_('Datetime Modified'), auto_now=True)
 
     def __str__(self):
         return self.title
@@ -37,7 +38,8 @@ class Comment(models.Model):
         ('5', _('Perfect')),
     ]
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments',
+                                verbose_name=_('Comment Product'))
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments',
                                verbose_name=_('Comment Author'))
     body = models.TextField(verbose_name=_('Comment Text :', ))
